c3201e3538b12d0da2a40394a0af87fce14438f6
Robert W. Harris Jr <roharris@us.ibm.com>
2017-05-02 10:18:43 -0500

    Intro solution to Issue #1

--- dev/components/App.jsx
+++ dev/components/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 
+import Counter from './Counter.js';
+
 export default class App extends React.Component {
   render() {
     return (
      <div style={{textAlign: 'center'}}>
         <h1>Show off!</h1>
-      </div>);
+	   <Counter />
+      </div>
+	 );
   }
 }
