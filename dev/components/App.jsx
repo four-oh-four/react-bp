38043d8600465ddfe5040eca3d85e7796a060d77
sandhya reddy <sandhya.reddy23@gmail.com>
2017-05-15 14:01:30 -0500

    Solution -Component that prints 1-10

--- dev/components/App.jsx
+++ dev/components/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import Counter from './counter.js';
 
 export default class App extends React.Component {
   render() {
     return (
      <div style={{textAlign: 'center'}}>
-        <h1>Show off!</h1>
+        <Counter/>
       </div>);
   }
 }
